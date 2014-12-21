b09374483e0d71ba24d19669c6498025a302ad22
Nickolai Novik <is.infinity@yahoo.com>
2014-12-22 00:06:11 +0200

    add exmpale

---
+++ examples/simple.py
@@ -0,0 +1,32 @@
+import asyncio
+import aiorwlock
+
+
+loop = asyncio.get_event_loop()
+
+
+@asyncio.coroutine
+def go():
+    rwlock = aiorwlock.RWLock(loop=loop)
+    # aquire reader lock
+    try:
+        yield from rwlock.reader_lock.acquire()
+        # or same way you can acquire reader lock
+        # yield from rwlock.reader_lock.acquire()
+        print("inside reader lock")
+        yield from asyncio.sleep(0.1, loop=loop)
+    finally:
+        yield from rwlock.reader_lock.release()
+
+    # acquire writer lock
+    try:
+        yield from rwlock.writer_lock.acquire()
+        # or same way you can acquire reader lock
+        # yield from rwlock.reader_lock.acquire()
+        print("inside writer lock")
+        yield from asyncio.sleep(0.1, loop=loop)
+    finally:
+        yield from rwlock.writer_lock.release()
+
+
+loop.run_until_complete(go())
