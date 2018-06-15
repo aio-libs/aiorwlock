0e75b9f5743c131f69690e71ca217c76deeb302f
ranyixu <1015243376@qq.com>
2018-06-15 17:29:43 +0800

    update comment format

--- aiorwlock/__init__.py
+++ aiorwlock/__init__.py
@@ -165,16 +165,14 @@ class _RWLockCore:
         self._wake_up()
 
     def _wake_up(self):
-        """If no one is reading or writing, wake up write waiters
-        first, only one write waiter should be waken up, if no
-        write waiters and have read waiters, wake up all read waiters.
-        """
+        # If no one is reading or writing, wake up write waiters
+        # first, only one write waiter should be waken up, if no
+        # write waiters and have read waiters, wake up all read 
+        # waiters.
         if self._r_state == 0 and self._w_state == 0:
             if self._write_waiters:
-                # Only wake up one write writer
                 self._wake_up_first(self._write_waiters)
             elif self._read_waiters:
-                # Wake up all read waiters
                 self._wake_up_all(self._read_waiters)
 
     def _wake_up_first(self, waiters):
